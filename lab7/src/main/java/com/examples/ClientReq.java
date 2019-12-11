452ff56d1f2c74994fc03c97b05d7610e4c924ba
nika <pinskaya.nika@mail.ru>
2019-12-11 21:47:52 +0300

    Scripted auto-commit on change (2019-12-11 21:47:52) by gitwatch.sh

--- lab7/src/main/java/com/examples/ClientReq.java
+++ lab7/src/main/java/com/examples/ClientReq.java
@@ -3,6 +3,19 @@ package com.examples;
 public class ClientReq {
     public static void main(String[] args) {
         ZContext context = new ZContext();
-        ZMQ
+        ZMQ.Socket socket = null;
+        try {
+            System.out.println("connect");
+            socket = context.createSocket(SocketType.REQ);
+            socket.connect("tcp://localhost:5555");
+            for (int i = 0; i < 10; i++) {
+                socket.sendd("request" + i, 0);
+                String reply = socket.recvStr();
+                System.out.println("reply" + i + " result=" + reply);
+            }
+        } finally {
+            context.destroySocket(socket);
+            context.destroy();
+        }
     }
 }
