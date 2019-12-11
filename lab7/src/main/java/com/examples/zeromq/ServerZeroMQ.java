7bdc98627348aa0212ade824a265074055b99edc
nika <pinskaya.nika@mail.ru>
2019-12-12 00:16:12 +0300

    Scripted auto-commit on change (2019-12-12 00:16:12) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/ServerZeroMQ.java
+++ lab7/src/main/java/com/examples/zeromq/ServerZeroMQ.java
@@ -1,28 +1,28 @@
-package com.examples.zeromq;
-
-public class ServerZeroMQ {
-    public static void main(String[] args) {
-
-//    Создаем ZContext или ZMQ.Context
-        ZContext context = new ZContext();
-        ZMQ.Socket socket = null;
-
-        try {
-            socket.bind("tcp://localhost:5555");
-            System.out.println("bind!");
-            while (!Thread.currentThread().isInterrupted()) {
-                String req = socket.recvStr();
-                socket.send("reply! + req);");
-            }
-        } finally {
-            context.destroySocket(socket);
-            context.destroy();
-        }
-// С помощью методов Context.socket(<тип сокета>) или ZContext.createSocket(<тип сокета>) создаем сокет
-//    В бесконечном цикле читаем из сокета данные и отвечаем или посылаем данные в другие сокеты
-//    Посылать можно набор байтов, строку и сообщение Zmsg(набор фреймов)
-//    Массив и строка посылаются методами сокета sendXXX, принимаются методами сокетов recvXXX
-//    ZMsg отправляется и своими методами ZMsg.send и ZMsg.recvMsg
-
-    }
-}
+//package com.examples.zeromq;
+//
+//public class ServerZeroMQ {
+//    public static void main(String[] args) {
+//
+////    Создаем ZContext или ZMQ.Context
+//        ZContext context = new ZContext();
+//        ZMQ.Socket socket = null;
+//
+//        try {
+//            socket.bind("tcp://localhost:5555");
+//            System.out.println("bind!");
+//            while (!Thread.currentThread().isInterrupted()) {
+//                String req = socket.recvStr();
+//                socket.send("reply! + req);");
+//            }
+//        } finally {
+//            context.destroySocket(socket);
+//            context.destroy();
+//        }
+//// С помощью методов Context.socket(<тип сокета>) или ZContext.createSocket(<тип сокета>) создаем сокет
+////    В бесконечном цикле читаем из сокета данные и отвечаем или посылаем данные в другие сокеты
+////    Посылать можно набор байтов, строку и сообщение Zmsg(набор фреймов)
+////    Массив и строка посылаются методами сокета sendXXX, принимаются методами сокетов recvXXX
+////    ZMsg отправляется и своими методами ZMsg.send и ZMsg.recvMsg
+//
+//    }
+//}
