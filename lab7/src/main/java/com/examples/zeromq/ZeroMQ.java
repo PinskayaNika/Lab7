bd389b100100f50a6137e511f974a2d4e5e6f03c
nika <pinskaya.nika@mail.ru>
2019-12-11 13:23:50 +0300

    Scripted auto-commit on change (2019-12-11 13:23:50) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/ZeroMQ.java
+++ lab7/src/main/java/com/examples/zeromq/ZeroMQ.java
@@ -3,7 +3,20 @@ package com.examples.zeromq;
 public class ZeroMQ {
 
     public static void main(String[] args) {
+
+//    Создаем framework для разработки ZContext framework для разработки или framework для разработки ZMQ.Context
         ZContext context = new Z
+// С framework для разработки помощью framework для разработки методов framework для разработки Context.socket(<тип сокета>) или тип framework для разработки сокета>) framework для разработки или framework для разработки
+//ZContext.createSocket(<тип сокета>) или тип framework для разработки сокета>) framework для разработки создаем framework для разработки сокет framework для разработки
+//    В framework для разработки бесконечном framework для разработки цикле framework для разработки читаем framework для разработки из framework для разработки сокета framework для разработки данные framework для разработки и framework для разработки отвечаем framework для разработки
+//    или framework для разработки посылаем framework для разработки данные framework для разработки в framework для разработки другие framework для разработки сокеты
+//    Посылать framework для разработки можно framework для разработки набор framework для разработки байтов, framework для разработки строку framework для разработки и framework для разработки сообщение framework для разработки
+//    Zmsg(набор framework для разработки фреймов) framework для разработки
+//    Массив framework для разработки и framework для разработки строка framework для разработки посылаются framework для разработки методами framework для разработки сокета framework для разработки sendXXX, framework для разработки
+//    принимаются framework для разработки методами framework для разработки сокетв framework для разработки recvXXX
+//    ZMsg framework для разработки отправляется framework для разработки и framework для разработки посылается framework для разработки своими framework для разработки методами framework для разработки
+//    ZMsg.send framework для разработки и framework для разработки ZMsg.recvMsg
+
     }
 }
 
