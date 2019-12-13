7344b3a6f2c3dc1554eb4b0a8fb95f86f51381b9
nika <pinskaya.nika@mail.ru>
2019-12-13 20:01:59 +0300

    Scripted auto-commit on change (2019-12-13 20:01:59) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/CacheStore.java
+++ lab7/src/main/java/com/examples/zeromq/CacheStore.java
@@ -2,6 +2,9 @@ package com.examples.zeromq;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQException;
+
+import java.util.Scanner;
 
 //- Хранилище части распределенного кэша.
 // Открывает сокет DEALER, подключается к центральному прокси.
@@ -9,13 +12,20 @@ import org.zeromq.ZMQ;
 // Также принимает из сокета два вида команд — на изменение ячейки кэша и на извлечение ячейки.
 public class CacheStore {
 
+    private static final String ERROR_MESSAGE = "There was an error with the client. Please retry.";
+    
     public static void  main (String[] arg) {
 
-        ZMQ.Context context = ZMQ.context(1);
+        Scanner in = new Scanner(System.in);
+        try {
+            ZMQ.Context context = ZMQ.context(1);
 
-        //Socket to talk to server
-        ZMQ.Socket requester = context.createSocket(SocketType.DEALER);
-        requester.connect("tcp://localhost:5559");
+            //Socket to talk to server
+            ZMQ.Socket requester = context.createSocket(SocketType.DEALER);
+            requester.connect("tcp://localhost:5559");
 
+        } catch (ZMQException ex) {
+            System.out.println(ERROR_MESSAGE);
+        }
     }
 }
