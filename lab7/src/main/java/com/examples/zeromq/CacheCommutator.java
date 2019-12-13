4b2d35de1d2780f5fcebd4b26dc6fdae4e1ea8b6
nika <pinskaya.nika@mail.ru>
2019-12-14 00:56:19 +0300

    Scripted auto-commit on change (2019-12-14 00:56:19) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/CacheCommutator.java
+++ lab7/src/main/java/com/examples/zeromq/CacheCommutator.java
@@ -19,4 +19,19 @@ public class CacheCommutator {
     public String getRightBound() {
         return rightBound;
     }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+    
+    public boolean isIntersect(String value) {
+        int val = Integer.parseInt(value);
+        int left = Integer.parseInt(leftBound);
+        int right = Integer.parseInt(rightBound);
+        return left <= val && val <= right ;
+    }
 }
