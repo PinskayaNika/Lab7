b5e707504f435f9f63dcc992370c12e3c4305f96
nika <pinskaya.nika@mail.ru>
2019-12-14 00:52:32 +0300

    Scripted auto-commit on change (2019-12-14 00:52:32) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/CacheCommutator.java
+++ lab7/src/main/java/com/examples/zeromq/CacheCommutator.java
@@ -1,4 +1,22 @@
 package com.examples.zeromq;
 
 public class CacheCommutator {
+
+    private String leftBound;
+    private String rightBound;
+    private long time;
+
+    public CacheCommutator (String left, String right, long time) {
+        leftBound = left;
+        rightBound = right;
+        this.time = time;
+    }
+
+    public String getLeftBound() {
+        return leftBound;
+    }
+
+    public String getRightBound() {
+        return rightBound;
+    }
 }
