228047cc3bfd50c93fa8835842c1b1156b2e4c53
nika <pinskaya.nika@mail.ru>
2019-12-13 19:51:13 +0300

    Scripted auto-commit on change (2019-12-13 19:51:13) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/CacheStore.java
+++ lab7/src/main/java/com/examples/zeromq/CacheStore.java
@@ -1,5 +1,6 @@
 package com.examples.zeromq;
 
+import org.zeromq.SocketType;
 import org.zeromq.ZMQ;
 
 //- Хранилище части распределенного кэша.
@@ -7,6 +8,9 @@ import org.zeromq.ZMQ;
 // После подключения с определнным интервалом времени высылает сообщение NOTIFY в котором сообщает интервал хранимых значений.
 // Также принимает из сокета два вида команд — на изменение ячейки кэша и на извлечение ячейки.
 public class CacheStore {
-    ZMQ.Context 
+    ZMQ.Context context = ZMQ.context(1);
+
+    //Socket to talk to server
+    ZMQ.Socket requester = context.socket(SocketType.DEALER);
 
 }
