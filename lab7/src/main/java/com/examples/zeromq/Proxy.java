5cad7efe5e4898457db04d6dc8d1858f8f8fecd7
nika <pinskaya.nika@mail.ru>
2019-12-14 00:58:16 +0300

    Scripted auto-commit on change (2019-12-14 00:58:16) by gitwatch.sh

--- lab7/src/main/java/com/examples/zeromq/Proxy.java
+++ lab7/src/main/java/com/examples/zeromq/Proxy.java
@@ -66,7 +66,7 @@ public class Proxy {
                 } else {
                     String[] data = message.getLast().toString().split(DELIMITER);
                     if (data[0].equals(GET_COMMAND)) {
-                        for (Map.Entry <ZFrame, C)
+                        for (Map.Entry <ZFrame, CacheCommutator>)
                     }
                 }
             }
